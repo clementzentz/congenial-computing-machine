4c12afab7101bcc5904e8bed2ac19ac595b72e04
Clément Zentz <clzt94@gmail.com>
2024-09-23 12:46:02 +0200

    Part 6: static files end. Add more test + refactor tests.

--- polls/views.py
+++ polls/views.py
@@ -7,45 +7,24 @@ from django.utils import timezone
 
 from .models import Choice, Question
 
-
-class IndexView(generic.ListView):
-    template_name = "polls/index.html"
-    context_object_name = "latest_question_list"
-
+class QuestionQuerySetMixin:
     def get_queryset(self):
-        """Return the last five published questions.
-        (not including those set to be published in the future)"""
-        return (Question.objects
-            .filter(pub_date__lte=timezone.now())
-            .order_by("-pub_date")[:5])
-
-
-class DetailView(generic.DetailView):
-    model = Question
-    template_name = "polls/detail.html"
-    def get_queryset(self):
-        """Excludes any questions 
+        """ Excludes any questions 
         that aren't published yet. """
-        return Question.objects.filter(
-            pub_date__lte=timezone.now())
-
-
-class ResultsView(generic.DetailView):
-    model = Question
-    template_name = "polls/results.html"
+        return (Question.objects
+                .exclude(choice__isnull=True)
+                .filter(pub_date__lte=timezone.now()))
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
-        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+        selected_choice = question.choice_set.get(
+            pk=request.POST["choice"])
     except (KeyError, Choice.DoesNotExist):
         # Redisplay the question voting form.
-        return render(
-            request,
-            "polls/detail.html",
-            {
-                "question": question,
-                "error_message": "You didn't select a choice.",
+        return render(request,"polls/detail.html", {
+            "question": question,
+            "error_message": "You didn't select a choice.",
             },
         )
     else:
@@ -54,4 +33,25 @@ def vote(request, question_id):
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
-        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+        return HttpResponseRedirect(
+            reverse("polls:results", args=(question.id,)))
+
+# IndexView inherit QuestionQuerySetMixin
+class IndexView(QuestionQuerySetMixin, generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+    def get_queryset(self):
+        """Return the last five published questions.
+        (not including those set to be published in the future)"""
+        return (super().get_queryset()
+                .order_by("-pub_date")[:5])
+    
+# DetailView inherit QuestionQuerySetMixin  
+class DetailView(QuestionQuerySetMixin, generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+# ResultsView inherit QuestionQuerySetMixin 
+class ResultsView(QuestionQuerySetMixin, generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
